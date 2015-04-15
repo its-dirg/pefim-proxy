6738b9646314b2f6875c6f4136a7b647ff84c92e
Hans Hörberg <hans.horberg@umu.se>
2015-04-15 16:51:13 +0200

    Added encryption om targetid.

---
+++ src/pefimproxy/util/targetid.py
@@ -0,0 +1,71 @@
+import json
+import os
+from uuid import uuid4
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
+from saml2.aes import AESCipher
+
+
+class TargetIdHandler(object):
+    AES_ALG = ["aes_128_cbc", "aes_128_cfb", "aes_128_ecb",
+               "aes_192_cbc", "aes_192_cfb", "aes_192_ecb",
+               "aes_256_cbc", "aes_256_cfb", "aes_256_ecb"]
+
+    def __init__(self, e_alg=None, key=None, h_alg=None, iv=None):
+
+        if e_alg is None:
+            self.e_alg = self.AES_ALG[0]
+        else:
+            self.e_alg = e_alg
+        assert self.e_alg in self.AES_ALG, "The encryption alg %s do not exist. Use one of the follwing encryption " \
+                                            "alg: %s" % (self.alg, self.AES_ALG)
+        typ, bits, cmode = self.e_alg.split("_")
+        if key is None:
+            self.key = os.urandom(int(bits) >> 3)
+        else:
+            self.key = key
+        assert len(self.key) == int(bits) >> 3, "The key must consist of %d bytes." % int(bits) >> 3
+        if h_alg is None:
+            self.h_alg = "sha256"
+        else:
+            self.h_alg = h_alg
+        if iv is None:
+            self.iv = Random.new().read(AES.block_size)
+        else:
+            self.iv = iv
+        assert len(self.iv) == AES.block_size, "The initialization vector must be %d size." % AES.block_size
+        h_alg_ok = False
+        approved_hash_alg = ""
+        for tmp_h_alg in hashlib.algorithms:
+            if self.h_alg == tmp_h_alg:
+                h_alg_ok = True
+            approved_hash_alg += tmp_h_alg + ","
+        assert h_alg_ok, "The hash alg %s do not exist. Use one of the following alg: %s" % (self.h_alg,
+                                                                                             approved_hash_alg)
+        self.aes = AESCipher(key=self.key, iv=self.iv)
+
+    def td2_json(self, tid1, sp_entityid):
+        td2_dict = {
+            "tid1": tid1,
+            "sp_entityid": sp_entityid,
+            "uuid": uuid4().urn
+        }
+        return json.dumps(td2_dict)
+
+    def td2_dict(self, tid2_json):
+        return json.loads(tid2_json)
+
+    def td2_encrypt(self, tid1, sp_entityid):
+        td2 = self.td2_json(tid1, sp_entityid)
+        td2_encrypt = self.aes.encrypt(msg=td2, alg=self.e_alg)
+        return td2_encrypt
+
+    def td2_decrypt(self, td2_encrypted):
+        td2_json = self.aes.decrypt(td2_encrypted, alg=self.e_alg)
+        return self.td2_dict(td2_json)
+
+    def td2_hash(self, tid1, sp_entityid):
+        td2 = self.td2_json(tid1, sp_entityid)
+        hash_func = getattr(hashlib, self.h_alg)
+        return hash_func(td2).hexdigest()
---
+++ tests/test_TargetIdTest.py
@@ -0,0 +1,101 @@
+import os
+from saml2.ident import IdentDB
+from pefimproxy.util.targetid import TargetIdHandler
+
+
+class TestTargetId():
+
+    def setup_class(self):
+        self.id = IdentDB({})
+        self.sp_entity_id = "https://localhost:/sp.xml"
+        sp_id = "urn:mace:umu.se:sp"
+        self.nameid1_text = "abcd0001"
+        self.nameid1 = self.id.persistent_nameid(self.nameid1_text, sp_id)
+
+    def get_aes_128_key(self):
+        return os.urandom(16)
+
+    def get_aes_192_key(self):
+        return os.urandom(24)
+
+    def get_aes_256_key(self):
+        return os.urandom(32)
+
+    def test_create_tid2_json(self):
+        tih = TargetIdHandler()
+        tid1 = self.nameid1.text.strip()
+        tid2_json = tih.td2_json(tid1, self.sp_entity_id)
+        tid2_dict = tih.td2_dict(tid2_json)
+        assert tid2_dict["tid1"] == self.nameid1_text, "Cannot verify tid1."
+        assert tid2_dict["sp_entityid"] == self.sp_entity_id, "Cannot verify SP entity id."
+        assert tid2_dict["uuid"] is not None and len(tid2_dict["uuid"]) > 0, "Cannot verify SP entity id."
+
+    def test_hash_tid1(self):
+        hash_verify = {'md5': 32, 'sha1': 40, 'sha224': 56, 'sha256': 64, 'sha384': 96, 'sha512': 128}
+        try:
+            TargetIdHandler(h_alg="do_not_exist_alg")
+            assert False, "Must be an assert since the alg. do not exist."
+        except:
+            pass
+
+        tih = TargetIdHandler()
+        tid1 = self.nameid1.text.strip()
+        tid2_hash = tih.td2_hash(tid1, self.sp_entity_id)
+        assert len(tid2_hash) == 64, "Default is sha256 alg and i should generate a string with length 64."
+
+        for tmp_h_alg in hash_verify:
+            tih = TargetIdHandler(h_alg=tmp_h_alg)
+            tid1 = self.nameid1.text.strip()
+            tid2_hash = tih.td2_hash(tid1, self.sp_entity_id)
+            assert len(tid2_hash) == hash_verify[tmp_h_alg], "%s alg should generate a string with length %d." % \
+                                         (tmp_h_alg, hash_verify[tmp_h_alg])
+
+    def test_encrypt_tid1(self):
+        tih = TargetIdHandler()
+        tid1 = self.nameid1.text.strip()
+        tid2_encrypted = tih.td2_encrypt(tid1, self.sp_entity_id)
+        tid2_dict = tih.td2_decrypt(tid2_encrypted)
+        assert tid2_dict["tid1"] == self.nameid1_text, "Cannot verify tid1."
+        assert tid2_dict["sp_entityid"] == self.sp_entity_id, "Cannot verify SP entity id."
+        assert tid2_dict["uuid"] is not None and len(tid2_dict["uuid"]) > 0, "Cannot verify SP entity id."
+
+    def test_encrypt_alg_1(self):
+        for alg in TargetIdHandler.AES_ALG:
+            typ, bits, cmode = alg.split("_")
+            tih = TargetIdHandler(e_alg=alg)
+            tid1 = self.nameid1.text.strip()
+            tid2_encrypted = tih.td2_encrypt(tid1, self.sp_entity_id)
+            tid2_dict = tih.td2_decrypt(tid2_encrypted)
+            assert tid2_dict["tid1"] == self.nameid1_text, "Cannot verify tid1."
+            assert tid2_dict["sp_entityid"] == self.sp_entity_id, "Cannot verify SP entity id."
+            assert tid2_dict["uuid"] is not None and len(tid2_dict["uuid"]) > 0, "Cannot verify SP entity id."
+
+    def test_encrypt_alg_2(self):
+        for alg in TargetIdHandler.AES_ALG:
+            typ, bits, cmode = alg.split("_")
+            iv = os.urandom(16)
+            key = os.urandom(int(bits) >> 3)
+            tih = TargetIdHandler(e_alg=alg, iv=iv, key=key)
+            tid1 = self.nameid1.text.strip()
+            tid2_encrypted = tih.td2_encrypt(tid1, self.sp_entity_id)
+            tid2_dict = tih.td2_decrypt(tid2_encrypted)
+            assert tid2_dict["tid1"] == self.nameid1_text, "Cannot verify tid1."
+            assert tid2_dict["sp_entityid"] == self.sp_entity_id, "Cannot verify SP entity id."
+            assert tid2_dict["uuid"] is not None and len(tid2_dict["uuid"]) > 0, "Cannot verify uuid."
+
+    def test_encrypt_alg_3(self):
+        for alg in TargetIdHandler.AES_ALG:
+            typ, bits, cmode = alg.split("_")
+            iv = os.urandom(16)
+            key = os.urandom(int(bits) >> 3)
+            tih = TargetIdHandler(e_alg=alg, iv=iv, key=key)
+            tid1 = self.nameid1.text.strip()
+            tid2_encrypted_1 = tih.td2_encrypt(tid1, self.sp_entity_id)
+            tid2_encrypted_2 = tih.td2_encrypt(tid1, self.sp_entity_id)
+            tid2_dict_1 = tih.td2_decrypt(tid2_encrypted_1)
+            tid2_dict_2 = tih.td2_decrypt(tid2_encrypted_2)
+            assert tid2_encrypted_1 != tid2_encrypted_2, "Two encryption of the same data must never be the same!"
+            assert tid2_dict_1["tid1"] == tid2_dict_2["tid1"] == self.nameid1_text, "Cannot verify tid1."
+            assert tid2_dict_1["sp_entityid"] == tid2_dict_2["sp_entityid"] == self.sp_entity_id, \
+                "Cannot verify SP entity id."
+            assert tid2_dict_1["uuid"] != tid2_dict_2["uuid"], "Cannot verify uuid."
